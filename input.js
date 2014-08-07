00f94c7f8ee0b57a589eba1fbae53739f8fc3297
David Gustafson <git@radicalskeptic.net>
2014-08-07 15:04:35 -0700

    Line editing now works properly.

--- input.js
+++ input.js
@@ -4,9 +4,11 @@ var keypress = require('keypress');
 
 // make `process.stdin` begin emitting "keypress" events
 keypress(process.stdin);
-//process.stdin.setEncoding( 'utf8' );
 
 var myOutput = "";
+
+// Sample command list for command completion.
+// in the future it will hold the commands available
 var myCommandList = ["apple",
                       "app",
                       "battery",
@@ -21,23 +23,32 @@ var completionArray = [];
 var completionArrayIndex = 0;
 var lastWordLength = 0;
 
+//Clears console output so that previous contents of myOutput does not show
 var clearCmd = function(){
   var clearChars = new Array(lastWordLength + 1).join( " " );
   process.stdout.write("\r" + clearChars + "\r");
 };
 
 var tabPressed = function(){
+  clearCmd();
+  //If tab has been pressed before
   if (lastKeyPressed === "tab") {
+    //and completionArray has contents
     if (completionArray.length > 0){
-      lastWordLength = myOutput.length;
       myOutput = completionArray[completionArrayIndex];
+      lastWordLength = myOutput.length;
+      //increment completionArrayIndex if the index is no larger than length.
       if (completionArray.length > completionArrayIndex + 1) {
         completionArrayIndex = completionArrayIndex + 1;
       } else {
+        //or reset index to 0
         completionArrayIndex = 0;
       }
     }
   } else {
+    //else if tab has not been pressed before
+    //filter out the commands tha begin with myOutput
+    completionArray = [];
     completionArrayIndex = 0;
     var tempArray = myCommandList.map(function(item){
       if (item.match(myOutput)) {
@@ -51,29 +62,39 @@ var tabPressed = function(){
   }
 };
 
+var getLongest = function(length){
+  if (length > lastWordLength){
+    return length;
+  } else {
+    return lastWordLength;
+  }
+};
+
 var processInput = function(ch, key){
   if (key && key.ctrl && key.name === 'c') {
     process.exit();
-  } else if (key.name === 'return'){
-    myCommandList.push(myOutput);
+  } else if (key && key.name === 'return'){
     console.log("\n");
-  } else if (key.name === 'tab') {
+  } else if (key && key.name === 'tab') {
     tabPressed();
-  } else if(key.name === 'backspace'){
+  } else if(key && key.name === 'backspace'){
+    clearCmd();
     myOutput = myOutput.slice(0, -1);
-  } else if(key.name === 'space'){
-    myOutput = myOutput + " ";
-  } else {
-    myOutput = myOutput + key.name;
     lastWordLength = myOutput.length;
+  } else if(key && key.name === 'space'){
+    myOutput = myOutput + " ";
+  } else if(key){
+    myOutput = myOutput + ch;
+    lastWordLength = getLongest(myOutput.length);
   }
-  lastKeyPressed = key.name;
+  if (key) { lastKeyPressed = key.name; }
 };
+
+console.log("CTRL-C to exit");
 
 process.stdin.on('keypress', function (ch, key) {
   processInput(ch, key);
-  //var clearChars = new Array(lastWordLength + 1).join( " " );
-  //process.stdout.write("\r" + clearChars + "\r");
+  lastWordLength = myOutput.length;
   clearCmd();
   process.stdout.write(myOutput);
 });
